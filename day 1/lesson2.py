8351cdc042d4460637d9663c31664157d881f120
M40Cairo <sdocommanndo@gmail.com>
2024-02-02 09:45:37 -0700

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,17 @@
+name = "saba"
+
+surname= "shalikashvili"
+
+height= 173
+
+age= 15
+
+sport= "frenburti"
+ 
+interest= "programming"
+
+print("chemi saxelia" + " " + (name) + ", " + "gvari" + " " + (surname) + ", " + "simagleshi var" + " " + str(height) + ", " + "var" + " " + str(age) + " " + "wlis" + ", " + "momwons" + " " + (sport) + ", " + "dainteresebuli var" + " " + (interest) + "-it")
+
+
+
+
